2c3079fcb5a1936aa66c22498fb79a8660b502b6
sohyeonPark <hidhen9714@naver.com>
2019-09-18 10:26:33 +0900

    add docs index

--- php_201840213/while02.php
+++ php_201840213/while02.php
@@ -1,6 +1,7 @@
 <?php
-$body = file_get_contents("for01.php");
+// $body = file_get_contents("for01.php");
 
+$filename = "for01.php";
 $fp = fopen($filename,"r");
 
 $count =0;
@@ -11,4 +12,4 @@ while(!feof($fp)){
    
 }
 fclose($fp);
-echo "소스에 $기호가 ".$count."있습니다";
+echo "소스에 $ 기호가".$count."있습니다";
